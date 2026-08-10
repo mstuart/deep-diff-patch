93b3fe06ffeec770f323b5df49660f8dd207c82a
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-09 22:37:34 -0700

    chore: migrate to Ultracite (#10)
    * chore: migrate to ultracite
    
    * chore: enforce ultracite
    
    ---------
    
    Co-authored-by: Mark Stuart <mstuart@users.noreply.github.com>

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,17 +1,16 @@
-import {expectType, expectError} from 'tsd';
-import {
-	diff,
-	patch,
-	applyPatch,
-	type Operation,
-} from './index.js';
+import { expectError, expectType } from "tsd";
+import { applyPatch, diff, type Operation, patch } from "./index.js";
 
-expectType<Operation[]>(diff({a: 1}, {a: 2}));
-expectType<Operation[]>(diff({}, {a: 1}));
-expectType<Operation[]>(diff({a: 1}, {}));
+expectType<Operation[]>(diff({ a: 1 }, { a: 2 }));
+expectType<Operation[]>(diff({}, { a: 1 }));
+expectType<Operation[]>(diff({ a: 1 }, {}));
 
-expectType<Record<string, unknown>>(patch({a: 1}, [{op: 'replace', path: '/a', value: 2}]));
-expectType<Record<string, unknown>>(applyPatch({a: 1}, [{op: 'add', path: '/b', value: 3}]));
+expectType<Record<string, unknown>>(
+  patch({ a: 1 }, [{ op: "replace", path: "/a", value: 2 }])
+);
+expectType<Record<string, unknown>>(
+  applyPatch({ a: 1 }, [{ op: "add", path: "/b", value: 3 }])
+);
 
-expectError(diff('not an object', {}));
-expectError(patch('not an object', []));
+expectError(diff("not an object", {}));
+expectError(patch("not an object", []));
